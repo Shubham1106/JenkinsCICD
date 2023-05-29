0b19f73c485db574c14a0faf7f0549c07ef671ca
Shubham1106 <59809984+Shubham1106@users.noreply.github.com>
2023-05-29 14:32:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,23 @@ variable "instanceCount" {
     description = "Please enter the number of instacne needs to be created"
   
 }
+
+variable "client_id" {
+    description =   "Client ID (APP ID) of the application"
+    type        =   string
+}
+
+variable "client_secret" {
+    description =   "Client Secret (Password) of the application"
+    type        =   string
+}
+
+variable "subscription_id" {
+    description =   "Subscription ID"
+    type        =   string
+}
+
+variable "tenant_id" {
+    description =   "Tenant ID"
+    type        =   string
+}
