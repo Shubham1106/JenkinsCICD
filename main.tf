e71739caa4acb11ee4da246251dd5b476e334636
Shubham1106 <59809984+Shubham1106@users.noreply.github.com>
2023-05-29 22:38:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "azurerm" {
-    version         =   "~> 2.0"
+    version         =   ">= 2.0"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
