bd20970c1dd3ae459f59056033baa00615f78617
Shubham1106 <59809984+Shubham1106@users.noreply.github.com>
2023-05-29 13:09:50 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+    default = "ShubhamG"
+    description = "name of the prefix"
+}
+
+variable "location" {
+    default = "eastus"
+    description = "name of the location of the resources"
+  
+}
+
+variable "instanceCount" {
+    description = "Please enter the number of instacne needs to be created"
+  
+}
